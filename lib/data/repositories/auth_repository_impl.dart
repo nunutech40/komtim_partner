4d3e807d931f02c0d3b425e4597e5ab2b854a2b9
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2023-07-10 15:34:24 +0700

    refactor repository and use mixin to reusable use executeEither

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -10,60 +10,19 @@ import 'package:komtim_partner/domain/entities/login_model.dart';
 import 'package:komtim_partner/domain/repositories/auth_repository.dart';
 
 import '../../common/exception.dart';
+import 'base_repository.dart';
 
-class AuthRepositoryImpl implements AuthRepository {
+class AuthRepositoryImpl with RepositoryMixin implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
   final SharedPref sharedPref;
 
   AuthRepositoryImpl(
-      {required this.remoteDataSource, required this.sharedPref}) {}
-  @override
-  Future<Either<Failure, bool>> getAuthState() async {
-    try {
-      bool isLoggedIn = await sharedPref.isLoggedIn();
-      return Right(isLoggedIn);
-    } catch (e) {
-      return Left(UnknownFailure('Unexpected Error: ${e.toString()}'));
-    }
-  }
+      {required this.remoteDataSource, required this.sharedPref});
 
   @override
-  Future<Either<Failure, LoginModel>> doLogin(
-      String username, String password) async {
+  Future<Either<Failure, T>> executeEither<T>(Future<T> Function() f) async {
     try {
-      // login and get data login, token and user
-      final result = await remoteDataSource.doLogin(username, password);
-
-      // save to preferences
-      sharedPref.saveUserAndToken(LoginResponse(
-          accessToken: result.accessToken,
-          tokenType: result.tokenType,
-          data: result.data));
-
-      return Right(result.toEntity());
-    } on ServerException catch (e) {
-      return Left(ServerFailure(e.message));
-    } on SocketException catch (e) {
-      return Left(
-          ConnectionFailure('Failed to connect to the network: ${e.message}'));
-    } catch (e) {
-      return Left(UnknownFailure('Unexpected Error: ${e.toString()}'));
-    }
-  }
-
-  @override
-  Future<Either<Failure, bool>> doLogout() async {
-    try {
-      // logout
-      sharedPref
-          .removeDataPref(); // ini kalau logout sudah jadi, harus di remove
-      final result = await remoteDataSource.doLogout();
-
-      // if logout success remove pref
-      if (result) {
-        sharedPref.removeDataPref();
-      }
-
+      final result = await f();
       return Right(result);
     } on ServerException catch (e) {
       return Left(ServerFailure(e.message));
@@ -73,5 +32,37 @@ class AuthRepositoryImpl implements AuthRepository {
     } catch (e) {
       return Left(UnknownFailure('Unexpected Error: ${e.toString()}'));
     }
+  }
+
+  @override
+  Future<Either<Failure, bool>> getAuthState() async {
+    return executeEither(() async {
+      return await sharedPref.isLoggedIn();
+    });
+  }
+
+  @override
+  Future<Either<Failure, LoginModel>> doLogin(
+      String username, String password) async {
+    return executeEither(() async {
+      final result = await remoteDataSource.doLogin(username, password);
+      sharedPref.saveUserAndToken(LoginResponse(
+          accessToken: result.accessToken,
+          tokenType: result.tokenType,
+          data: result.data));
+      return result.toEntity();
+    });
+  }
+
+  @override
+  Future<Either<Failure, bool>> doLogout() async {
+    return executeEither(() async {
+      sharedPref.removeDataPref();
+      final result = await remoteDataSource.doLogout();
+      if (result) {
+        sharedPref.removeDataPref();
+      }
+      return result;
+    });
   }
 }
---
+++ lib/data/repositories/base_repository.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:komtim_partner/common/failure.dart';
+
+import '../../common/exception.dart';
+
+mixin RepositoryMixin {
+  Future<Either<Failure, T>> executeEither<T>(Future<T> Function() f) async {
+    try {
+      final result = await f();
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(e.message));
+    } on SocketException catch (e) {
+      return Left(
+          ConnectionFailure('Failed to connect to the network: ${e.message}'));
+    } catch (e) {
+      return Left(UnknownFailure('Unexpected Error: ${e.toString()}'));
+    }
+  }
+}
